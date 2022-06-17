61a971b7e6d198dd7da0007391038b8cfa86824c
M0710Fa <b1018188.fun@gmail.com>
2022-06-17 15:40:59 +0900

    fix readme and add files

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # analysis-codes
+## fileMaps
+```mermaid
+  graph LR;
+      id1{root} --- id2[analysis-codes]
+        id2 --- id4(predict_uasge)
+            id4 --- id5>codes]
+
+      id1 --- id3[analysis-datas]
+        id3 --- id6(src)
+            id6 --- id7>sources]
+        id3 --- id8(tests)
+            id8 --- id9>test datas]
+        id3 --- id10(results)
+            id10 --- id11>results datas]
+```
---
+++ predict_usage/multipleRegression_sklearn.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler, scale
+
+df = pd.read_csv("datas/dfix.csv")
+
+ # 説明変数
+x = pd.get_dummies(df[["app","week","time"]],drop_first=True)
+x1 = [["app"]]
+x2 = [["week"]]
+x3 = [["time"]]
+
+# 目的変数
+y = df[["used"]]
+
+scaler = StandardScaler()
+scaler.fit(x)
+x_std = scaler.transform(x)
+scaler.fit(y)
+y_std = scaler.transform(y)
+
+model = LinearRegression()
+model.fit(x_std, y_std)
+print(model.coef_)
+print(model.score(x_std, y_std))
+
+dt = pd.read_csv("datas/test.csv")
+test_x = pd.get_dummies(df[["app","week","time"]],drop_first=True)
+test_y = df[["used"]]
+
+pred = scaler.inverse_transform(model.predict(test_x))
+print(pred)
