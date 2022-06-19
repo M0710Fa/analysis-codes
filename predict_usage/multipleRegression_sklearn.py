d9e59df19f477d7a6d47635d75bb8c5a97aec0c3
M0710Fa <b1018188.fun@gmail.com>
2022-06-20 02:17:12 +0900

    [wip]rmseでぎない

--- predict_usage/multipleRegression_sklearn.py
+++ predict_usage/multipleRegression_sklearn.py
@@ -1,6 +1,9 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import StandardScaler, scale
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("../analysis-datas/src/fixed_usage.csv")
 
@@ -21,12 +24,17 @@ y_std = scaler.transform(y)
 
 model = LinearRegression()
 model.fit(x_std, y_std)
-print(model.coef_)
-print(model.score(x_std, y_std))
+
 
 dt = pd.read_csv("../analysis-datas/tests/test-1day.csv")
-test_x = pd.get_dummies(df[["app","week","time"]],drop_first=True)
-test_y = df[["used"]]
+test_x = pd.get_dummies(dt[["app","week","time"]],drop_first=True)
+test_y = dt[["used"]]
 
 pred = scaler.inverse_transform(model.predict(test_x))
-print(pred)
+# true_y = test_y.values
+# rmse = np.sqrt(mean_squared_error(true_y, pred))
+
+# print("重回帰モデル　coef {}".format(model.coef_))
+# print("予測の精度　{}".format(model.score(x_std, y_std)))
+# print(x)
+# print("rmse_result　{}".format(rmse))
