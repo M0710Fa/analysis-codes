d72a306f8361578bcaa3032510a54e49c34a9aac
M0710Fa <b1018188.fun@gmail.com>
2022-07-19 03:47:49 +0900

    [feat]交差検証の調節

--- predict_usage/multipleRegression_sklearn.py
+++ predict_usage/multipleRegression_sklearn.py
@@ -12,13 +12,14 @@ df = pd.read_csv("../analysis-datas/src/usage_intest.csv")
 x = pd.get_dummies(df[["app","week","time"]],drop_first=True)
 X = np.array(x)
 # 目的変数
-Y = np.array(df["used"])
+y = df["used"]
+Y = np.array(y)
 
 # データの分割(訓練データとテストデータ)
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=0)
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, random_state=0)
 
 model = LinearRegression()
-model.fit(X, Y)
+model.fit(x_train, y_train)
 
 
 # 結果
@@ -26,10 +27,10 @@ print("【切片】:", int(model.intercept_))
 print(*np.array(model.coef_,dtype="i8"),sep=',')
 # print("【決定係数(テスト)】:", model.score(X_test, Y_test))
 
-# pred = model.predict(X_test)
-# rmse = np.sqrt(mean_squared_error(Y_test, pred))
-# print(pred)
-# print(int(rmse))
+pred = model.predict(x_test)
+rmse = np.sqrt(mean_squared_error(y_test, pred))
+print(pred)
+print(int(rmse))
 # print(*np.array(pred,dtype="i8"),sep=',')
-# rmse_msec  = datetime.timedelta(milliseconds=int(rmse))
-# print("rmse（分）：{}".format(rmse_msec.seconds/60))
+rmse_msec  = datetime.timedelta(milliseconds=int(rmse))
+print("rmse（分）：{}".format(rmse_msec.seconds/60))
--- predict_usage/multipleRegression_statsmodels.py
+++ predict_usage/multipleRegression_statsmodels.py
@@ -15,7 +15,7 @@ X = np.array(x)
 Y = np.array(df["used"])
 
 # データの分割(訓練データとテストデータ)
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=0)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.9, random_state=0)
 
 # 定数項(y切片)を必要とする線形回帰のモデル式ならば必須
 Xsm = sm.add_constant(X)
@@ -27,17 +27,17 @@ result = model.fit()
 # 重回帰分析の結果を表示する
 print(result.summary())
 
-# Xst = sm.add_constant(X_test)
-# pred = result.predict(Xst)
-# rmse = np.sqrt(mean_squared_error(Y_test,pred))
+Xst = sm.add_constant(X_test)
+pred = result.predict(Xst)
+rmse = np.sqrt(mean_squared_error(Y_test,pred))
 # print(rmse)
 # print(pred)
 # print(*np.array(pred,dtype="i8"),sep=',')
-# rmse_msec  = datetime.timedelta(milliseconds=rmse)
-# print("rmse（分）：{}".format(rmse_msec.seconds/60))
+rmse_msec  = datetime.timedelta(milliseconds=rmse)
+print("rmse（分）：{}".format(rmse_msec.seconds/60))
 
 #print(*result.params, sep=",")
-print(*np.array(result.params,dtype="i8"),sep=',')
+# print(*np.array(result.params,dtype="i8"),sep=',')
 
 
 # dt = pd.read_csv('../analysis-datas/tests/test-1day.csv')
